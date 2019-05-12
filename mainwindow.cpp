d3316fd025429990b9adde9a49b53fcc02040750
bzxc <48920950+bzxc@users.noreply.github.com>
2019-05-12 21:35:35 +0800

    Create mainwindow.cpp

---
+++ mainwindow.cpp
@@ -0,0 +1,45 @@
+#include <QGraphicsView>
+#include <QTimer>
+
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      scene(new QGraphicsScene(this)),
+      view(new QGraphicsView(scene, this)),
+      game(new GameController(*scene, this))
+{
+    setCentralWidget(view);
+    resize(600, 600);
+
+    initScene();
+    initSceneBackground();//初始化场景和背景
+
+    QTimer::singleShot(0, this, SLOT(adjustViewSize()));
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::adjustViewSize()
+{
+    view->fitInView(scene->sceneRect(), Qt::KeepAspectRatioByExpanding);
+}
+
+void MainWindow::initScene()
+{
+    scene->setSceneRect(-100, -100, 200, 200);
+}
+
+void MainWindow::initSceneBackground()
+{
+    QPixmap bg(TILE_SIZE, TILE_SIZE);
+    QPainter p(&bg);
+    p.setBrush(QColor(40, 70, 70));
+    p.drawRect(0, 0, TILE_SIZE, TILE_SIZE);
+
+    view->setBackgroundBrush(QBrush(bg));
+}
